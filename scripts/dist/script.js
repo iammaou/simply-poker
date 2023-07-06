dfc201db5519be1feb57f91b5aedeb124048f93f
iammaou <markokubica625@gmail.com>
2023-07-06 13:54:40 -0700

    am done with work for today, gjala helped fix an issue I had lika a chad so thats fun

--- scripts/dist/script.js
+++ scripts/dist/script.js
@@ -6,30 +6,28 @@ const d4 = document.querySelector('.dealer-card-4');
 const d5 = document.querySelector('.dealer-card-5');
 const h1 = document.querySelector('.card-1');
 const h2 = document.querySelector('.card-2');
-const o1 = document.querySelector('.opponent-card-1');
-const o2 = document.querySelector('.opponent-card-2');
 const raiseButton = document.querySelector('.raise-button');
 const callButton = document.querySelector('.call-button');
 const foldButton = document.querySelector('.fold-button');
+const value = document.querySelector('.personalValue');
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52];
 const start = [1, 14, 27, 40];
-function findAttr(a) {
+let cardValue = 0;
+function findAttr(amp) {
     let cardNumbers = [];
-    if (a <= 12) {
+    if (amp <= 12) {
         for (let i = 0; i < start.length; i++) {
-            cardNumbers[i] = start[i] + a;
+            cardNumbers[i] = start[i] + amp;
         }
         return cardNumbers;
     }
-    else {
-        return 0;
-    }
+    return [];
 }
-function findType(a) {
+function findType(amp) {
     let cardNumbers = [];
-    if (a <= 4) {
+    if (amp <= 4) {
         for (let i = 0; i < 13; i++) {
-            cardNumbers[i] = cards[i + (a * 13)];
+            cardNumbers[i] = cards[i + (amp * 13)];
         }
         return cardNumbers;
     }
@@ -37,16 +35,16 @@ function findType(a) {
         return 0;
     }
 }
-function findColor(a) {
+function findColor(amp) {
     let cardNumbers = [];
-    if (a == 1 || a == 2) {
+    if (amp == 1 || amp == 2) {
         for (let i = 0; i < cards.length / 2; i++) {
             if (i == cards.length / 4) {
-                a += 1;
-                cardNumbers[i] = cards[i + ((a - 1) * 13)];
+                amp += 1;
+                cardNumbers[i] = cards[i + ((amp - 1) * 13)];
             }
             else {
-                cardNumbers[i] = cards[i + ((a - 1) * 13)];
+                cardNumbers[i] = cards[i + ((amp - 1) * 13)];
             }
         }
         return cardNumbers;
@@ -76,12 +74,8 @@ d4.src = `../../assets/PNG-cards-1.3/PNG-cards-1.3/${dealer[3]}.png`;
 d5.src = `../../assets/PNG-cards-1.3/PNG-cards-1.3/${dealer[4]}.png`;
 h1.src = `../../assets/PNG-cards-1.3/PNG-cards-1.3/${hand[0]}.png`;
 h2.src = `../../assets/PNG-cards-1.3/PNG-cards-1.3/${hand[1]}.png`;
-o1.src = `../../assets/PNG-cards-1.3/PNG-cards-1.3/${opponent[0]}.png`;
-o2.src = `../../assets/PNG-cards-1.3/PNG-cards-1.3/${opponent[1]}.png`;
-function abc() {
-    const arr1 = ['bobby', 'hadz'];
-    const arr2 = [1, 2];
-    const arr3 = [...arr1, ...arr2];
-    console.log(arr3);
+let a = findAttr(1);
+if (a.includes(2)) {
+    value.textContent = 'High Card';
 }
 //# sourceMappingURL=script.js.map
--- scripts/src/script.ts
+++ scripts/src/script.ts
@@ -7,63 +7,58 @@ const d5:any = document.querySelector('.dealer-card-5');
 const h1:any = document.querySelector('.card-1');
 const h2:any = document.querySelector('.card-2');
 
-const o1:any = document.querySelector('.opponent-card-1');
-const o2:any = document.querySelector('.opponent-card-2');
+// const o1:any = document.querySelector('.opponent-card-1');
+// const o2:any = document.querySelector('.opponent-card-2');
 
 const raiseButton:any = document.querySelector('.raise-button');
 const callButton:any = document.querySelector('.call-button');
 const foldButton:any = document.querySelector('.fold-button');
+const value:any = document.querySelector('.personalValue');
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52];
 const start = [1, 14, 27, 40];
+let cardValue:Number = 0;
 
-
-function findAttr(a:number){ //returns: kings, queens, jacks, 10-2 and aces
+function findAttr(amp:number){ //returns: kings, queens, jacks, 10-2 and aces
     let cardNumbers = [];
     
-    if(a<=12){
+    if(amp<=12){
         for(let i=0; i<start.length; i++){
-            cardNumbers[i] = start[i] + a;
+            cardNumbers[i] = start[i] + amp;
         }
         return cardNumbers;
-    } else{
-        return 0;
-    }
+    } 
+    return [];
 }
 
-function findType(a:number){ //returns: spades, hearts, clubs, diamonds
+function findType(amp:number){ //returns: spades, hearts, clubs, diamonds
     let cardNumbers = [];
 
-    if(a<=4){
+    if(amp<=4){
         for(let i=0; i<13; i++){
-            cardNumbers[i] = cards[i+(a*13)];
+            cardNumbers[i] = cards[i+(amp*13)];
         }
         return cardNumbers;
-    } else {
-        return 0;
-    }
+    } 
+    return [];
 }
 
-function findColor(a:number){ //returns: black, red
+function findColor(amp:number){ //returns: black, red
     let cardNumbers = [];
     
-    if(a==1 || a==2){
+    if(amp==1 || amp==2){
         for(let i=0; i<cards.length/2; i++){
             if(i==cards.length/4){
-                a+=1;
-                cardNumbers[i] = cards[i+((a-1)*13)];
+                amp+=1;
+                cardNumbers[i] = cards[i+((amp-1)*13)];
             } else{
-                cardNumbers[i] = cards[i+((a-1)*13)];
+                cardNumbers[i] = cards[i+((amp-1)*13)];
             }
         }
         return cardNumbers;
-    } else{
-        return 0;
     }
+    return [];
 }
-
-// black 1-13 & 27-39 - 1
-// red 14-26 & 40-52 - 2
 
 let cardsCopy = [...cards];
 
@@ -91,16 +86,9 @@ d5.src = `../../assets/PNG-cards-1.3/PNG-cards-1.3/${dealer[4]}.png`;
 h1.src = `../../assets/PNG-cards-1.3/PNG-cards-1.3/${hand[0]}.png`;
 h2.src = `../../assets/PNG-cards-1.3/PNG-cards-1.3/${hand[1]}.png`;
 
-o1.src = `../../assets/PNG-cards-1.3/PNG-cards-1.3/${opponent[0]}.png`;
-o2.src = `../../assets/PNG-cards-1.3/PNG-cards-1.3/${opponent[1]}.png`;
+// o1.src = `../../assets/PNG-cards-1.3/PNG-cards-1.3/${opponent[0]}.png`;
+// o2.src = `../../assets/PNG-cards-1.3/PNG-cards-1.3/${opponent[1]}.png`;
 
-function abc(){
-    const arr1 = ['bobby', 'hadz'];
-    const arr2 = [1, 2];
-
-    // 👇️ const arr3: (string | number)[]
-    const arr3 = [...arr1, ...arr2];
-
-    // 👇️ [ 'bobby', 'hadz', 1, 2 ]
-    console.log(arr3);
+if(findAttr(1).includes(2)){
+    value.textContent = 'High Card';
 }
