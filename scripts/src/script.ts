1a70b67715adefcc825bed3d90dbc7de7575c94d
iammaou <markokubica625@gmail.com>
2023-07-06 11:50:09 -0700

    made find functions that Ill use to set the rules later on

--- scripts/src/script.ts
+++ scripts/src/script.ts
@@ -1,62 +1,3 @@
-/*
-const cards = [
-    1 King Spades
-    2 Queen Spades
-    3 Jack Spades
-    4 10 Spades
-    5 9 Spades
-    6 8 Spades
-    7 7 Spades
-    8 6 Spades
-    9 5 Spades
-    10 4 Spades
-    11 3 Spades
-    12 2 Spades
-    13 Ace Spades
-
-    14 King Hearts 
-    15 Queen Hearts
-    16 Jack Hearts 
-    17 10 Hearts 
-    18 9 Hearts 
-    19 8 Hearts 
-    20 7 Hearts 
-    21 6 Hearts 
-    22 5 Hearts 
-    23 4 Hearts 
-    24 3 Hearts 
-    25 2 Hearts 
-    26 Ace Hearts 
-
-    27 King Clubs 
-    28 Queen Clubs 
-    29 Jack Clubs 
-    30 10 Clubs 
-    31 9 Clubs 
-    32 8 Clubs 
-    33 7 Clubs 
-    34 6 Clubs 
-    35 5 Clubs 
-    36 4 Clubs 
-    37 3 Clubs 
-    38 2 Clubs 
-    39 Ace Clubs 
-
-    40 King Diamonds 
-    41 Queen Diamonds 
-    42 Jack Diamonds 
-    43 10 Diamonds 
-    44 9 Diamonds 
-    45 8 Diamonds 
-    46 7 Diamonds 
-    47 6 Diamonds 
-    48 5 Diamonds 
-    49 4 Diamonds 
-    50 3 Diamonds 
-    51 2 Diamonds 
-    52 Ace Diamonds
-];*/
-
 const d1:any = document.querySelector('.dealer-card-1');
 const d2:any = document.querySelector('.dealer-card-2');
 const d3:any = document.querySelector('.dealer-card-3');
@@ -74,6 +15,56 @@ const callButton:any = document.querySelector('.call-button');
 const foldButton:any = document.querySelector('.fold-button');
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52];
+const start = [1, 14, 27, 40];
+
+
+function findAttr(a:number){ //returns: kings, queens, jacks, 10-2 and aces
+    let cardNumbers = [];
+    
+    if(a<=12){
+        for(let i=0; i<start.length; i++){
+            cardNumbers[i] = start[i] + a;
+        }
+        return cardNumbers;
+    } else{
+        return 0;
+    }
+}
+
+function findType(a:number){ //returns: spades, hearts, clubs, diamonds
+    let cardNumbers = [];
+
+    if(a<=4){
+        for(let i=0; i<13; i++){
+            cardNumbers[i] = cards[i+(a*13)];
+        }
+        return cardNumbers;
+    } else {
+        return 0;
+    }
+}
+
+function findColor(a:number){ //returns: black, red
+    let cardNumbers = [];
+    
+    if(a==1 || a==2){
+        for(let i=0; i<cards.length/2; i++){
+            if(i==cards.length/4){
+                a+=1;
+                cardNumbers[i] = cards[i+((a-1)*13)];
+            } else{
+                cardNumbers[i] = cards[i+((a-1)*13)];
+            }
+        }
+
+        return cardNumbers;
+    } else{
+        return 0;
+    }
+}
+
+// black 1-13 & 27-39 - 1
+// red 14-26 & 40-52 - 2
 
 let cardsCopy = [...cards];
 
@@ -103,3 +94,14 @@ h2.src = `../../assets/PNG-cards-1.3/PNG-cards-1.3/${hand[1]}.png`;
 
 o1.src = `../../assets/PNG-cards-1.3/PNG-cards-1.3/${opponent[0]}.png`;
 o2.src = `../../assets/PNG-cards-1.3/PNG-cards-1.3/${opponent[1]}.png`;
+
+function abc(){
+    const arr1 = ['bobby', 'hadz'];
+    const arr2 = [1, 2];
+
+    // 👇️ const arr3: (string | number)[]
+    const arr3 = [...arr1, ...arr2];
+
+    // 👇️ [ 'bobby', 'hadz', 1, 2 ]
+    console.log(arr3);
+}
